702dc92b4ea76c68c2d83ea35bb00dca7a338dbc
Isikyus <edward@palarang.com>
2016-11-08 22:17:25 +1100

    Actually attempt to load a given YAML file
    And accept input as a filename or on stdin

--- aeldardin-to-dot.rb
+++ aeldardin-to-dot.rb
@@ -2,6 +2,8 @@
 #
 # Script to convert Aeldardin-Key YAML files into .dot files representing the connection graph of the dungeon.
 
+require 'yaml'
+
 class AeldardinToDot
     
     # @param [IO] file The IO object to read YAML from.
@@ -11,8 +13,26 @@ class AeldardinToDot
     
     # @param [IO] output_file The IO object to write the output 'dot' graph data to.
     def generateDot(output_file)
-        output_file.write(@file.readline)
+        
+        # Load the whole dungeon into memory at once.
+        # TODO: this will fail for sufficently large dungeons.
+
+        # 'nil' means read all lines as one row.
+        yaml_text = @file.readlines(nil)[0]
+        yaml_data = YAML.load(yaml_text)
+
+        output_file.write(yaml_data.keys)
     end
 end
 
-AeldardinToDot.new(STDIN).generateDot(STDOUT)
+case ARGV.length
+when 0
+    input_file = STDIN
+when 1
+    input_file = File.open(ARGV[1], 'r')
+else
+    STDERR.puts "Usage: #{ARGV[0]} [<filename.yml>]"
+    exit 1
+end
+
+AeldardinToDot.new(input_file).generateDot(STDOUT)
