efe7f4753083bd251f335570e9bc7bcfb862ea32
Isikyus <edward@palarang.com>
2016-11-12 00:49:58 +1100

    Factor out dungeon model to its own class
    To support doing things other than rendering to .gv

--- aeldardin-to-dot.rb
+++ aeldardin-to-dot.rb
@@ -2,98 +2,55 @@
 #
 # Script to convert Aeldardin-Key YAML files into Dot .gv files representing the connection graph of the dungeon.
 
-require 'yaml'
+module Aeldardin
+    class ToDot
 
-class AeldardinToDot
-
-    # @param [IO] file The IO object to read YAML from.
-    def initialize(file)
-        @file = file
-    end
-
-    # @param [IO] output_file The IO object to write the output .gv graph data into.
-    def generateDot(output_file)
-
-        # Output .gv header:
-        output_file.puts("graph #{safe_id(dungeon['title'])} {")
-
-        rooms_by_key.each do |_key, room|
-
-            room_name = room['name'] || room['key']
-
-            # Output a graph edge for every exit from the room.
-            # TODO: We will end up duplicating edges.
-            room['exits'].each do |exit|
-
-                # Exit may look like '42' or { secret: '42' }
-                dest_key = exit.is_a?(Hash) ? exit.values.first : exit
-
-                # Use the name, rather than key, of the destination if possible.
-                # This has to match the name we use for this node elsewhere (i.e. for its exits).
-                dest_name = rooms_by_key[dest_key] && rooms_by_key[dest_key]['name'] || dest_key
-
-                output_file.puts("    #{node_name(room_name)} -- #{node_name(dest_name)};")
-            end
+        # @param [Aeldardin::Dungeon] dungeon The dungeon to render to Dot format.
+        def initialize(dungeon)
+            @dungeon = dungeon
         end
 
-        # Conclude graph data
-        output_file.puts('}')
-    end
+        # @param [IO] output_file The IO object to write the output .gv graph data into.
+        def generateDot(output_file)
 
-    private
+            # Output .gv header:
+            output_file.puts("graph #{safe_id(@dungeon.title)} {")
 
-    def dungeon
-       @dungeon ||= begin
+            @dungeon.rooms_by_key.each do |_key, room|
 
-            # Load the whole dungeon into memory at once.
-            # TODO: this will fail for sufficently large dungeons.
+                room_name = room['name'] || room['key']
 
-            # 'nil' means read all lines as one row.
-            yaml_text = @file.readlines(nil)[0]
-            YAML.load(yaml_text)
-        end
-    end
+                # Output a graph edge for every exit from the room.
+                # TODO: We will end up duplicating edges.
+                room['exits'].each do |exit|
 
-    # Returns a lookup table from room keys (usually numbers) to the full room details.
-    def rooms_by_key
-        @rooms_by_key ||= begin
-            by_key = {}
+                    # Exit may look like '42' or { secret: '42' }
+                    dest_key = exit.is_a?(Hash) ? exit.values.first : exit
 
-            dungeon['zones'].each do |zone|
+                    # Use the name, rather than key, of the destination if possible.
+                    # This has to match the name we use for this node elsewhere (i.e. for its exits).
+                    dest_name = @dungeon.rooms_by_key[dest_key] && @dungeon.rooms_by_key[dest_key]['name'] || dest_key
 
-                regions = zone['regions']
-
-                if regions
-                    regions.each do |region|
-                        region['rooms'].each do |room|
-
-                            key = room['key']
-
-                            if by_key.key?(key)
-                                raise "Unexpected duplicate room #{key}: #{room.inspect}"
-                            end
-
-                            by_key[key] = room
-                        end
-                    end
-                else
-                    $stderr.puts("Found zone with no regions: #{zone['name']}")
+                    output_file.puts("    #{node_name(room_name)} -- #{node_name(dest_name)};")
                 end
             end
 
-            by_key
+            # Conclude graph data
+            output_file.puts('}')
         end
-    end
 
-    # Converts a key into a node name usable by dot (i.e. not starting with numbers).
-    def node_name(key)
-       "node_#{safe_id(key.to_s)}"
-    end
+        private
 
-    # Convert any string into something safe to use as a Dot ID (node or graph).
-    # Not guaranteed to preserve uniqueness.
-    def safe_id(string)
-        string.gsub(/\s+/, '_').gsub(/[^A-Za-z0-9_]/, '')
+        # Converts a key into a node name usable by dot (i.e. not starting with numbers).
+        def node_name(key)
+        "node_#{safe_id(key.to_s)}"
+        end
+
+        # Convert any string into something safe to use as a Dot ID (node or graph).
+        # Not guaranteed to preserve uniqueness.
+        def safe_id(string)
+            string.gsub(/\s+/, '_').gsub(/[^A-Za-z0-9_]/, '')
+        end
     end
 end
 
@@ -107,4 +64,7 @@ else
     exit 1
 end
 
-AeldardinToDot.new(input_file).generateDot(STDOUT)
+require_relative 'dungeon'
+
+dungeon = Aeldardin::Dungeon.load(input_file)
+Aeldardin::ToDot.new(dungeon).generateDot(STDOUT)
