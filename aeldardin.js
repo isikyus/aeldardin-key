2cdb9e5e1bcbf9952987c67e9404edc3527c8eca
Isikyus <edward@palarang.com>
2017-01-09 23:01:41 +1100

    Load JS and pass to toDot function
    Actually converting to .dot not yet implemented

--- aeldardin.js
+++ aeldardin.js
@@ -2,20 +2,66 @@
 //
 // Script to process an Aeldardin-Key YAML file as directed by its options.
 
-var showUsage = function() {
-  var command = process.argv0 + ' ' + process.argv[1];
-  console.warn('Usage:')
-  console.warn('  ' + command + ' <subcommand> [file]')
+// Using require.js so I can share modules with Aeldardin Rooms in future.
+// This code is from the require.js docs (http://requirejs.org/docs/node.html)
+var requirejs = require('requirejs');
 
-  // TODO: generate list of allowed subcommands.
-}
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
 
-var convertToDot = function(input) {
-  console.warn('TODO: not yet implemented');
-};
+requirejs([
+    'js-yaml',
+    'fs', // TODO: do I actually need this?
+    'to_dot'
+  ],
+function (yaml, fs, toDot) {
 
-// Wrap everything in a function for scoping
-var run = function() {
+  var showUsage = function() {
+    var command = process.argv0 + ' ' + process.argv[1];
+    console.warn('Usage:')
+    console.warn('  ' + command + ' <subcommand> [file]')
+
+    // TODO: generate list of allowed subcommands.
+  }
+
+  // Load a key from the given file, and pass it to the callback.
+  var loadKeyFromFile = function(filename, callback) {
+    fs.readFile(filename, 'utf8', function(err, data) {
+
+      var key;
+
+      if (err) {
+        throw err
+      } else {
+
+        try {
+          key = toDot(yaml.safeLoad(data));
+          callback(key);
+
+        } catch(e) {
+          if (e instanceof yaml.YAMLException) {
+            // TODO: should I pass the exception to the callback instead?
+            console.error('Error loading YAML from "' + filename + '"');
+            console.error(e.message);
+          } else {
+
+            // Don't try to handle any other kind of exception.
+            throw e;
+          }
+        }
+      }
+    });
+  }
+
+  var convertToDot = function(filename) {
+    loadKeyFromFile(filename, function(key) {
+      toDot(key);
+    });
+  };
 
   // Parse command line arguments.
   // Ignore the first two arguments ('node' and filename -- see http://stackoverflow.com/questions/4351521/how-do-i-pass-command-line-arguments)
@@ -27,13 +73,12 @@ var run = function() {
   switch(subcommand) {
 
     case 'dot':
-      convertToDot(input);
+      convertToDot(filename);
       break;
 
     default:
-      console.warn('Unrecognised subcommand ' + subcommand);
+      console.error('Unrecognised subcommand ' + subcommand);
       showUsage();
   }
-};
 
-run();
+});
---
+++ to_dot.js
@@ -0,0 +1,10 @@
+// File: to_dot.js
+//
+// Code to convert an Aeldardin key into a .dot graph file.
+
+define([],
+function () {
+  return function(data) {
+    console.dir(data);
+  };
+});
