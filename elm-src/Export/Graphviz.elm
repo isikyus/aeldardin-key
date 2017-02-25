0031c9969dc5778af2a254e8e5c131dd77a70a10
Isikyus <edward@palarang.com>
2017-02-25 20:04:17 +1100

    Include room-to-room edges in Graphviz output
    Still malformed, and we want to use node _names_
    rather than keys, but it's starting to look right now.

--- elm-src/Export/Graphviz.elm
+++ elm-src/Export/Graphviz.elm
@@ -6,4 +6,25 @@ import Dungeon exposing (..)
 
 toGraphviz : Dungeon -> String
 toGraphviz dungeon =
-  "graph " ++ dungeon.title ++ " { }"
+  "graph " ++ dungeon.title ++ " {\n" ++
+  ( List.foldl
+      String.append
+      ""
+      ( List.concatMap
+        toEdges
+        (Dungeon.rooms dungeon)
+      )
+  ) ++
+  "}"
+
+-- Convert a the exits of a room to a series of strings representing Graphviz edges.
+toEdges : Room -> List String
+toEdges room =
+  List.map
+    (\exit -> edge room.key exit.destination)
+    room.exits
+
+-- Generate Graphviz notation for an edge between two named nodes.
+edge : String -> String -> String
+edge start end =
+  "    " ++ start ++ " -- " ++ end ++ ";\n"
