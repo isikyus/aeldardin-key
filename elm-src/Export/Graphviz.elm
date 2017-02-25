bfadfe74ca4a68e4665cc24268d3aaa1e4d2c97f
Isikyus <edward@palarang.com>
2017-02-25 20:14:48 +1100

    Add rudimentary node-name escaping for .gv
    Still not up to the Ruby code's functionality
    (notably, we don't handle non-identifier .gv characters except space),
    but should now actually produce working graphs.

--- elm-src/Export/Graphviz.elm
+++ elm-src/Export/Graphviz.elm
@@ -3,10 +3,9 @@ module Export.Graphviz exposing (toGraphviz)
 import Dungeon exposing (..)
 
 -- Convert a dungeon to Graphviz's .gv format (which can then be rendered as an image)
-
 toGraphviz : Dungeon -> String
 toGraphviz dungeon =
-  "graph " ++ dungeon.title ++ " {\n" ++
+  "graph " ++ (toIdentifier dungeon.title) ++ " {\n" ++
   ( List.foldl
       String.append
       ""
@@ -17,6 +16,7 @@ toGraphviz dungeon =
   ) ++
   "}"
 
+
 -- Convert a the exits of a room to a series of strings representing Graphviz edges.
 toEdges : Room -> List String
 toEdges room =
@@ -24,7 +24,21 @@ toEdges room =
     (\exit -> edge room.key exit.destination)
     room.exits
 
+
 -- Generate Graphviz notation for an edge between two named nodes.
 edge : String -> String -> String
 edge start end =
   "    " ++ start ++ " -- " ++ end ++ ";\n"
+
+-- Convert a string to a valid Graphviz identifier,
+-- by replacing spaces with underscores.
+--
+-- Modified from the String.map example at http://package.elm-lang.org/packages/elm-lang/core/latest/String ,
+-- which is copyright Evan Czaplicki.
+-- TODO: should also handle other non-identifier characters.
+-- TODO: not guaranteed to preserve uniqueness
+toIdentifier : String -> String
+toIdentifier string =
+  String.map
+    (\c -> if c == ' ' then '_' else c)
+    string
