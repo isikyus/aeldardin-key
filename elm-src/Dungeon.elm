7d2042148b0b544726e0e5cfc0caf8f84a496e1b
Isikyus <edward@palarang.com>
2017-02-25 19:57:57 +1100

    Load rooms and their fields from JSON

--- elm-src/Dungeon.elm
+++ elm-src/Dungeon.elm
@@ -1,4 +1,4 @@
-module Dungeon exposing (Dungeon, Room, Zone, Regions(..))
+module Dungeon exposing (Dungeon, Room, Zone, Regions(..), Connection)
 
 -- Types representing an Aeldardin dungeon.
 -- These may eventually need to be parametric or something to allow addons with their own types.
@@ -17,4 +17,12 @@ type Regions =
   Regions (List Zone)
 
 type alias Room =
-  {}
+  { key : String
+  , name: String
+  , exits: List Connection
+  }
+
+type alias Connection =
+  { doorType : String
+  , destination : String
+  }
--- elm-src/Dungeon/ParseJson.elm
+++ elm-src/Dungeon/ParseJson.elm
@@ -6,16 +6,63 @@ module Dungeon.ParseJson exposing (decodeDungeon)
 import Dungeon exposing (..)
 import Json.Decode exposing (..)
 
-dungeon = map2 Dungeon
-        (field "title" string)
-        (field "zones" (list zone))
+dungeon =
+  map2 Dungeon
+    (field "title" string)
+    (field "zones" (list zone))
 
-zone = map2 Zone
-        (field "rooms" (list room))
-        (oneOf [ field "regions" regions, succeed (Regions []) ])
+zone =
+  map2 Zone
+    (field "rooms" (list room))
+    (oneOf
+      [ field "regions" regions
+      , succeed (Regions [])
+      ]
+    )
 
 regions = map Regions (list (lazy (\_ -> zone)))
-room = succeed Room
+
+room =
+  map3 Room
+    (field "key" string)
+    (field "name" string)
+    (field "exits" (list exit))
+
+
+-- Helper function: decode a one-element list to that one element, and fail on other lists.
+unwrapSingleton : List a -> Decoder a
+unwrapSingleton list =
+  case list of
+    [] ->
+
+      fail "Expected a one-field object for door type, got an empty one"
+
+    [ element ] ->
+      succeed element
+
+    first :: second :: rest ->
+      fail "Expected a one-field object for door type, got two or more fields"
+
+-- Exits can be a bare <key>, or a pair like <type>: <key>, where <type>
+-- is an arbitrary string describing how the exit works (door, secret, "magical portal", etc.)
+-- In the former case, the type is assumed to be "door".
+exit =
+  oneOf
+    [ map2 Connection (succeed "door") string
+    , let
+
+        -- Build a connection from a pair of arguments.
+        -- TODO: surely there's a cleaner way to do this?
+        connectionFromPair : ( String, String ) -> Connection
+        connectionFromPair (doorType, dest) =
+                Connection doorType dest
+      in
+        map
+          connectionFromPair
+          ( keyValuePairs string
+            |> andThen unwrapSingleton
+          )
+    ]
 
 decodeDungeon : String -> Result String Dungeon
 decodeDungeon jsonString =
