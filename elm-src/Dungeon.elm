37698312073740da94785066740622debcee7af9
Isikyus <edward@palarang.com>
2017-02-25 15:00:48 +1100

    Load more of Dungeon data structure
    Not doing anything with it yet

--- elm-src/Dungeon.elm
+++ elm-src/Dungeon.elm
@@ -1,7 +1,20 @@
-module Dungeon exposing (Dungeon)
+module Dungeon exposing (Dungeon, Room, Zone, Regions(..))
 
 -- Types representing an Aeldardin dungeon.
 -- These may eventually need to be parametric or something to allow addons with their own types.
 
 type alias Dungeon =
-  { title : String }
+  { title : String
+  , zones : List Zone
+  }
+
+type alias Zone =
+  { rooms : List Room
+  , regions : Regions
+  }
+
+type Regions =
+  Regions (List Zone)
+
+type alias Room =
+  {}
--- elm-src/Dungeon/ParseJson.elm
+++ elm-src/Dungeon/ParseJson.elm
@@ -3,11 +3,20 @@ module Dungeon.ParseJson exposing (decodeDungeon)
 -- Code that knows how to parse dungeons from JSON,
 -- including resolving the shorthand syntaxes we allow in the raw YAML key.
 
-import Dungeon exposing (Dungeon)
+import Dungeon exposing (..)
 import Json.Decode exposing (..)
 
-dungeonDecoder = map Dungeon ( field "title" string )
+dungeon = map2 Dungeon
+        (field "title" string)
+        (field "zones" (list zone))
+
+zone = map2 Zone
+        (field "rooms" (list room))
+        (oneOf [ field "regions" regions, succeed (Regions []) ])
+
+regions = map Regions (list (lazy (\_ -> zone)))
+room = succeed Room
 
 decodeDungeon : String -> Result String Dungeon
 decodeDungeon jsonString =
-  decodeString dungeonDecoder jsonString
+  decodeString dungeon jsonString
