22334645d279656f1562c7205c12db764d5e1b08
Isikyus <edward@palarang.com>
2017-09-07 14:09:24 +1000

    Support specifying stream for heading matcher too

--- features/step_definitions/html_output_steps.rb
+++ features/step_definitions/html_output_steps.rb
@@ -22,10 +22,10 @@ Then /the (output|stdout|stderr) should have the title "(.*)"/ do |stream, title
   expect(last_command_started).to have_output an_output_string_including(title)
 end
 
-Then /the output should have a section headed "(.*)"/ do |heading|
+Then /the (output|stdout|stderr) should have a section headed "(.*)"/ do |stream, heading|
   all_headings = %w[ h1 h2 h3 h4 h5 h6 ]
   all_section_headings = all_headings.map { |heading| "section #{heading}" }
   section_any_heading  = all_section_headings.join(', ')
 
-  expect(last_command_started).to output_html_matching have_selector(section_any_heading, :text => heading)
+  expect(last_command_started).to output_html_matching stream, have_selector(section_any_heading, :text => heading)
 end
